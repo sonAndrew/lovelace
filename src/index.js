677547b19412ca792d740b011b7e4a3409dee437
alexanderasche <alexander.asche@gmail.com>
2017-07-31 13:21:45 -0400

    googel API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import BaseLayout from './components/BaseLayout';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BaseLayout />, document.getElementById('root'));
+// Import Geocode Module
+import GG_Geocode from 'node-geocoder';
+const GG_API = 'AIzaSyAkU61SJoxhqnZyY1lNi6GcQXzlGS-GaRU';
+
+//  Geocode Settings
+const options = {
+  provider: 'google',
+  httpAdapter: 'https',
+  apiKey: GG_API, 
+  formatter: null
+}; 
+const geocoder = GG_Geocode(options);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLocation('Charleston');
+  }
+  handleLocation = (search_term) => {
+    geocoder.geocode(search_term)
+    .then(function(res) {
+      console.log(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
+  render() {
+    return(
+      <div>
+        Test
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
